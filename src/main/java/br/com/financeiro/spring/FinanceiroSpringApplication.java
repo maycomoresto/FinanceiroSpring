d2f9c212a7f928e4cee5b745c7d1f92209775a21
maycomoresto <maycomoresto@gmail.com>
2018-06-07 07:14:17 -0300

    Criação do pojeto

---
+++ src/main/java/br/com/financeiro/spring/FinanceiroSpringApplication.java
@@ -0,0 +1,12 @@
+package br.com.financeiro.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceiroSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceiroSpringApplication.class, args);
+	}
+}
